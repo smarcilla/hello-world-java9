8da0e1250be48fd764023208f4cb8d3ca279401e
Sergio Marcilla <sermara70@gmail.com>
2018-03-03 19:55:34 +0100

    Classic example of the Hello World created with java 9 modules.
    - Created the HelloWorld module and packaged in a jar following the java 9 module convention.
    - Created the HelloWorldClient module in charge of testing the correct functioning of the HelloWorld module.

---
+++ com.hello.client/com/hello/client/HelloWorldClient.java
@@ -0,0 +1,16 @@
+package com.hello.client;
+
+import com.hello.*;
+
+public class HelloWorldClient {
+
+	public static void main (String arg[]){
+
+		HelloWorld hello = new HelloWorld();
+
+		System.out.println(hello.sayHelloWorld());
+	}	
+}
+
+
+
